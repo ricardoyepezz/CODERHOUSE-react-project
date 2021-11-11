cf56f43716fad0bf2aed45cb05967a6000939801
Ricardo Yepez <ricardoyepezz@gmail.com>
2021-11-11 19:28:22 -0300

    Se cambia texto y enlace

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<p>Coderhouse project in progress</p>
+				<p>Coderhouse project in progress..!</p>
 				<a
 					className="App-link"
-					href="https://reactjs.org"
+					href="https://github.com/ricardoyepezz/cursoreact"
 					target="_blank"
 					rel="noopener noreferrer"
 				>
-					Learn React
+					Github!
 				</a>
 			</header>
 		</div>
